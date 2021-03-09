1ccbd3f452a3a1e8d4f0933caee0bb8311cc9cd1
Thibault Doucet <thibault.doucet44@gmail.com>
2021-03-09 13:08:19 +0100

    :sparkles: Add axios

---
+++ docs/precache-manifest.8667e5e076cc8f03888c89e3129a062b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d54bc00cc318f6b6e28",
+    "url": "/vue-app-2/css/app.5c44f2b0.css"
+  },
+  {
+    "revision": "45062fc4a58adbc6ca880efeb0aea65c",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "7d54bc00cc318f6b6e28",
+    "url": "/vue-app-2/js/app.b6272d37.js"
+  },
+  {
+    "revision": "fcb42d615147b18ba007",
+    "url": "/vue-app-2/js/chunk-vendors.454dd97f.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
